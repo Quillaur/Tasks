5de1f73cafc91cd9c539f94932b621154aea2a2c
Quillaur <quill@DESKTOP-LDUM88R>
2016-10-28 03:24:38 +0300

    Add project files.

---
+++ Tasks/Program.vb
@@ -0,0 +1,27 @@
+﻿''' <summary>
+''' The main module of the project.
+''' </summary>
+Module Program
+
+    ''' <summary>
+    ''' The entry point to the program.
+    ''' </summary>
+    Sub Main()
+        Do
+            Dim a, b As Byte
+
+            Do
+                Console.WriteLine("Enter A value: ")
+            Loop Until (Byte.TryParse(Console.ReadLine(), a))
+
+            Do
+                Console.WriteLine("Enter B value: ")
+            Loop Until (Byte.TryParse(Console.ReadLine(), b))
+
+            Console.WriteLine($"The sum of A and B equals: {a + b}")
+
+            Console.WriteLine("Press any key to continue or Escape to exit.")
+        Loop While (Console.ReadKey(True).Key <> ConsoleKey.Escape)
+    End Sub
+
+End Module
