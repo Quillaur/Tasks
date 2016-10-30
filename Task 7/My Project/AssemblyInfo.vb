fc0870544f151d5c9d47c6092da17cde225a3817
Quillaur <quillaur@outlook.com>
2016-10-30 08:33:14 +0300

    Solved seventh task.

---
+++ Task 7/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Task 7")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Task 7")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("51012758-e0bc-4241-9503-b5c43515e2a5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Task 7/Program.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Numerics
+
+''' <summary>
+''' The main module of the project.
+''' </summary>
+Module Program
+
+    ''' <summary>
+    ''' The entry point to the program.
+    ''' </summary>
+    Sub Main()
+        Do
+            Dim first, second, third As BigInteger
+
+            first = New BigInteger()
+
+            Do
+                Console.Write("Enter first number: ")
+            Loop Until (BigInteger.TryParse(Console.ReadLine(), first))
+
+            second = New BigInteger()
+
+            Do
+                Console.Write("Enter second number: ")
+            Loop Until (BigInteger.TryParse(Console.ReadLine(), second))
+
+            third = New BigInteger()
+
+            Do
+                Console.Write("Enter third number: ")
+            Loop Until (BigInteger.TryParse(Console.ReadLine(), third))
+
+            If (first >= second) Then
+                If (first >= third) Then
+                    Console.WriteLine($"The biggest number: {first}")
+                Else
+                    Console.WriteLine($"The biggest number: {third}")
+                End If
+            Else
+                If (second >= third) Then
+                    Console.WriteLine($"The biggest number: {second}")
+                Else
+                    Console.WriteLine($"The biggest number: {third}")
+                End If
+            End If
+
+            Console.WriteLine("Press any key to continue or Escape to exit.")
+        Loop While (Console.ReadKey(True).Key <> ConsoleKey.Escape)
+    End Sub
+
+End Module
